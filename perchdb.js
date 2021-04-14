7238df92707868dedcc1d07c263520a3c47cc7ec
Neil Stevens <neil@stevensmedia.us>
2021-04-14 19:05:01 -0400

    Bye bye couch.ts, hello perchdb.js

---
+++ perchdb.js
@@ -0,0 +1,30 @@
+export function client(opts) {
+	return {
+		// "Public"
+		opts: function() {
+			return opts
+		},
+		rest: function(path, method = "GET", data = null) {
+			if(typeof data == "object") {
+				data = JSON.stringify(data)
+			}
+			var url = this.optsURL()
+			url.path = path
+			return fetch(url.toString(), {
+				method: method,
+				body: data,
+			})
+		},
+
+		// "Private"
+		optsURL: function() {
+			const url = new URL('http://localhost:5984')
+			url.protocol = opts.protocol || 'http'
+			url.hostname = opts.hostname || 'localhost'
+			url.port = opts.port || 5984
+			url.username = opts.username
+			url.password = opts.password
+			return url
+		},
+	}
+}
