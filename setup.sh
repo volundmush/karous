c1e2448c33d3598b8cbe4c251c246a2c3b0b7090
Neil Stevens <neil@stevensmedia.us>
2021-04-13 23:36:14 -0400

    Docker set up script for CouchDB

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+USERNAME=karous
+PASSWORD=karous
+DOCKER=karous-couchdb
+
+debug()
+{
+	echo "\033[1;38;5;184m$1\033[0m"
+}
+
+curl()
+{
+	docker exec -it "$DOCKER" curl -u "$USERNAME:$PASSWORD" -X PUT "$1"
+}
+
+debug "Pulling latest CouchDB image"
+docker image pull couchdb:latest
+debug "Running CouchDB container $DOCKER"
+docker run -d --name "$DOCKER" -e COUCHDB_USER="$USERNAME" -e COUCHDB_PASSWORD="$PASSWORD" couchdb:latest
+debug "Waiting for CouchDB to start"
+while ! docker exec -it "$DOCKER" curl -u "$USERNAME:$PASSWORD" "http://127.0.0.1:5984/_config/vendor/version";
+	do sleep 1
+done
+debug "Initializing CouchDB"
+curl "http://127.0.0.1:5984/_users"
+curl "http://127.0.0.1:5984/_replicator"
+curl "http://127.0.0.1:5984/_global_changes"
+debug "Done!"
